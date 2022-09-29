d38089a6fd8cb7e071c9ee48ec8d7589c124e6b2
DavidWisdom <88563609+DavidWisdom@users.noreply.github.com>
2022-09-29 19:48:20 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,16 +2,34 @@
 // Created by DavidWisdom on 2022/9/28.
 //
 #include <iostream>
-#include <vector>
-#include "src/bst.h"
+#include "src/lqueue.h"
 using namespace std;
 using namespace DataStructures;
 int main() {
     LQueue<int> q;
+    q.enqueue(1);
     q.enqueue(2);
-    q.clear();
-    cout << q.size() << endl;
+    cout << "The front of the queue is " << q.front() << endl;
+    // The front of the queue is 1
     q.enqueue(3);
-    cout << q.front() << endl;
+    q.dequeue();
+    q.dequeue();
+    cout << "The front of the queue is " << q.front() << endl;
+    // The front of the queue is 3
+    cout << "The back of the queue is " << q.back() << endl;
+    // The back of the queue is 3
+    cout << "The size of the queue is " << q.size() << endl;
+    // The size of the queue is 1
+    q.clear();
+    cout << "The size of the queue is " << q.size() << endl;
+    // The size of the queue is 0
+    q.enqueue(4);
+    q.enqueue(5);
+    cout << "The front of the queue is " << q.front() << endl;
+    // The front of the queue is 4
+    cout << "The back of the queue is " << q.back() << endl;
+    // The back of the queue is 5
+    cout << "The size of the queue is " << q.size() << endl;
+    // The size of the queue is 2
     return 0;
 }
