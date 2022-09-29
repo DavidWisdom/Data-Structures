d977c7a7c17e02474e6ead23d578e0990670aaaa
DavidWisdom <88563609+DavidWisdom@users.noreply.github.com>
2022-09-29 19:16:51 +0800

    update

---
+++ src/bit.h
@@ -0,0 +1,15 @@
+//
+// Created by DavidWisdom on 2022/9/29.
+//
+
+#ifndef DATA_STRUCTURES_BIT_H
+#define DATA_STRUCTURES_BIT_H
+namespace DataStructures {
+    // 树状数组
+    template <typename T>
+    class BIT {
+    public:
+
+    };
+}
+#endif //DATA_STRUCTURES_BIT_H
---
+++ src/trie.h
@@ -0,0 +1,14 @@
+//
+// Created by DavidWisdom on 2022/9/29.
+//
+
+#ifndef DATA_STRUCTURES_TRIE_H
+#define DATA_STRUCTURES_TRIE_H
+namespace DataStructures {
+    // 字典树
+    template <typename T>
+    class Trie {
+
+    };
+}
+#endif //DATA_STRUCTURES_TRIE_H
